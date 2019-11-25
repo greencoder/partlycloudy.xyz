bc8a60a77cc9054ff7c6b7623ea7ea67cf26dce3
Scott Newman <snewman18@gmail.com>
2019-11-25 11:15:35 -0700

    Remove satellite image

--- src/components/Radar.vue
+++ src/components/Radar.vue
@@ -1,9 +1,8 @@
 <template>
   <section id="radar">
-    <header>Radar and Satellite Images</header>
+    <header>Radar Image</header>
     <main>
       <a v-bind:href="radar.href"><img v-bind:src="radar.icon" /></a>
-      <a v-bind:href="satellite.href"><img v-bind:src="satellite.icon" /></a>
     </main>
   </section>
 </template>
